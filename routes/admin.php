7bb7af5814136d103cd0000ee2e7f07f82a5ecd2
metacharset8 <iurasukhenko783@gmail.com>
2023-02-23 15:17:11 +0300

    admin page, adin auth page added

---
+++ routes/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login_process', [AuthController::class, 'login'])->name('login_process');
+
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::middleware("auth:admin")->group(function(){
+  Route::resource('posts', PostController::class);
+});
